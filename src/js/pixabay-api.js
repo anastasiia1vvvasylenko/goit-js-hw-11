7bc6381f392ebeaaa311322e7c7305cf1cf3b476
AnastasiiaV <166251420+anastasiia1vvvasylenko@users.noreply.github.com>
2024-08-27 21:53:45 +0200

    ghg

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -20,4 +20,33 @@ export function fetchPhotos(query) {
     safesearch: 'true',
   });
   document.querySelector('.loader').classList.remove('hidden');
+
+  return fetch(`https://pixabay.com/api/?${params}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+      }
+      return data.hits;
+    })
+    .catch(error => {
+      console.log(error);
+      iziToast.error({
+        message:
+          'An error occurred while fetching images. Please try again later.',
+        position: 'topRight',
+      });
+    })
+    .finally(() => {
+      document.querySelector('.loader').classList.add('hidden');
+    });
 }
